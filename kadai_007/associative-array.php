50440f79abc25cb8a8106b2757dceef5e463f7b0
KS <140215561+skeing@users.noreply.github.com>
2024-06-04 07:44:45 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>連想配列を作って値とキーを出力しよう</title>
+</head>
+<body>
+<?php
+$array = array(
+    'name' => "onion",
+    'price' => 200,
+    'weight'=> 160
+);
+
+print_r($array);
+?>
+</body>
+</html>
